ef3b0572e528d70f387391d9a275250fc815883f
Josefine Pettersson <pettersson.josefine@live.com>
2024-11-26 12:45:24 +0100

    Redirect to login page from landing page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,11 +1,19 @@
+import { useNavigate } from "react-router-dom";
+
 const LandingPage = () => {
+    const navigate = useNavigate();
+
+    const handleLogin = () => {
+        navigate("/login");
+    };
+
     return (
       <div className="container">
         <div className="logo-container">
           <img src="my-logo" alt="Golden tune logo" className="logo" />
           <p>Your music quiz app</p>
         </div>
-        <button className="button">Log in</button>
+        <button className="button" onClick={handleLogin}>Log in</button>
       </div>
     );
   };
