3a24103b4a9664727398421fb86fc194f135a480
Josefine Pettersson <pettersson.josefine@live.com>
2024-12-16 15:35:17 +0100

    Minor changes inte firebase.ts for deploy reasons

--- backend/src/firebase.ts
+++ backend/src/firebase.ts
@@ -1,12 +1,17 @@
-import admin, { ServiceAccount } from "firebase-admin";
-import serviceAccount from './serviceAccountKey.json';
+import * as admin from "firebase-admin";
 import dotenv from 'dotenv';
 
 dotenv.config();
 
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n')
+};
+
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount as ServiceAccount),
+    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
   });
 } else {
   console.log('Firebase admin already initialized');
